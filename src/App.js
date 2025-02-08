868e6a381e58e632f00dca41ca1d9632c6ec911b
daniel <122282403+danielma-tic@users.noreply.github.com>
2025-02-08 12:39:58 +0200

    Update App.js to use CabinetConfigurator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CabinetConfigurator from './components/CabinetConfigurator';
+
+function App() {
+  return (
+    <div className="App">
+      <CabinetConfigurator />
+    </div>
+  );
+}
+
+export default App;
